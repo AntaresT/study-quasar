52a2f06cc4d704b75e738b5385ed966a93786150
AntaresT <luizeduardovbe@gmail.com>
2021-10-29 15:45:33 -0300

    feature: added posts pages

--- src/pages/posts/PostForm.vue
+++ src/pages/posts/PostForm.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    <h1></h1>
-  </div>
+  <q-page>
+    <div class="q-pa-md text-h3">
+      Post
+    </div>
+    <div class="q-pa-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md" >
+        <q-input filled v-model="title" label="Title" lazy-rules :rules="fieldRules" />
+        <q-input filled type="textarea" v-model="postContent" label="Content" lazy-rules :rules="fieldRules"/>
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+    </div>
+  </q-page>
 </template>
 
 <script>
+
+import { ref } from 'vue'
+
 export default {
-  name: 'PostForm'
+
+  computed: {
+    checkRoute () {
+      console.log(this.$route)
+      return this.$route
+    },
+
+    fieldRules (value) {
+      return [value => (value && value.length > 0) || 'Please type something']
+    }
+  },
+
+  setup () {
+    const title = ref(null)
+    const postContent = ref(null)
+
+    return {
+      title,
+      postContent,
+
+      onSubmit () {
+        // TODO Chamada de API para inserir o autor
+        // if (accept.value !== true) {
+        //   $q.notify({
+        //     color: 'red-5',
+        //     textColor: 'white',
+        //     icon: 'warning',
+        //     message: 'You need to accept the license and terms first'
+        //   })
+        // } else {
+        //   $q.notify({
+        //     color: 'green-4',
+        //     textColor: 'white',
+        //     icon: 'cloud_done',
+        //     message: 'Submitted'
+        //   })
+        // }
+      },
+
+      onReset () {
+        title.value = null
+        postContent.value = null
+      }
+    }
+  }
 }
+
 </script>
--- src/pages/posts/PostList.vue
+++ src/pages/posts/PostList.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    <h1></h1>
-  </div>
+  <q-page>
+    <div class="q-pa-md">
+      <q-btn :to="{ name: 'PostCreate' , params: { mode:'new' }}" label="Novo Post" />
+    </div>
+    <div class="q-pa-md q-gutter-md">
+      <div v-for="(post,index) in posts" :key="index">
+        <q-card class="my-card">
+          <q-card-section>
+            <div class="text-h6">{{post.title}}</div>
+            <div class="text-subtitle2">by {{post.author}}</div>
+          </q-card-section>
+          <q-separator dark inset />
+          <q-card-section>
+            <span>{{post.content}}</span>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PostList'
+  data () {
+    return {
+      posts: [
+        { title: 'Titulo 1', author: 'author 1', content: 'laraarara' },
+        { title: 'Titulo 2', author: 'author 2', content: 'laraarara' }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
