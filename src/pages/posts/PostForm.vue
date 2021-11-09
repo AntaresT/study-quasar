e50b91f704556bfd782952b22ef1bf1cd8627b44
AntaresT <luizeduardovbe@gmail.com>
2021-11-09 12:16:24 -0300

    ajusted post form and list layout

--- src/pages/posts/PostForm.vue
+++ src/pages/posts/PostForm.vue
@@ -1,17 +1,23 @@
 <template>
-  <q-page>
-    <div class="q-pa-md text-h3">
-      Post
-    </div>
-    <div class="q-pa-md" style="max-width: 400px">
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md" >
-        <q-input filled v-model="title" label="Title" lazy-rules :rules="fieldRules" />
-        <q-input filled type="textarea" v-model="postContent" label="Content" lazy-rules :rules="fieldRules"/>
-        <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
-          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-        </div>
-      </q-form>
+  <q-page class="row justify-center">
+    <div class="col col-md-8 col-sm-10 col-xs-12">
+      <div class="q-pa-md text-h3">
+        Post
+      </div>
+      <div class="q-pa-md">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md" >
+          <q-input filled v-model="post.title" label="Title" lazy-rules :rules="fieldRules" />
+          <q-select filled v-model="post.category" :options="options" label="Category" />
+          <q-input filled v-model="post.description" label="Description" lazy-rules :rules="fieldRules" />
+          <q-input filled v-model="post.author" label="Author" lazy-rules :rules="fieldRules" />
+          <q-input filled type="textarea" v-model="post.content" label="Content" lazy-rules :rules="fieldRules"/>
+          <q-uploader url="http://localhost:4444/upload" color="blue" label="Image" flat bordered style="max-width: 300px" />
+          <div>
+            <q-btn label="Submit" type="submit" color="primary"/>
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+          </div>
+        </q-form>
+      </div>
     </div>
   </q-page>
 </template>
@@ -21,6 +27,23 @@
 import { ref } from 'vue'
 
 export default {
+
+  data () {
+    return {
+      post: {
+        category: '',
+        title: '',
+        description: '',
+        image: '',
+        content: '',
+        author: ''
+      },
+      options: [
+        'Movies', 'Music', 'Science', 'Art'
+      ],
+      persistent: false
+    }
+  },
 
   computed: {
     checkRoute () {
